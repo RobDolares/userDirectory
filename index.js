97143e9ba11c6eed83a7a3a8ab567c2bbfff6684
Robert Howington <zerorob2787@gmail.com>
2017-07-25 20:35:29 -0400

    initial commit - still have stylign for single bios

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const data = require('./data');
+const app = express();
+
+//tell express to use mustache
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+//static files
+app.use(express.static('public'));
+
+app.get('/home', function(req, res) {
+  res.render('home', data)
+})
+
+app.get('/bio/:user', function(req, res) {
+  let userName = req.params.user;
+  for (let i = 0; i < data.users.length; i++) {
+    if (data.users[i].username === userName) {
+      res.render('bio', data.users[i]);
+    }
+  }
+});
+console.log(data)
+
+app.listen(3000, function() {
+  console.log('Successfully started express application!');
+})
+
+
+// app.get('/food/:fruit', function(req, res){
+//
+//   res.render('fruits', {
+//     fruit: req.params.fruit
+//   });
+//
+// })
